ee35e4988ac0dac9f6bdfe494f23110afea6c718
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-21 10:40:41 +0200

    uppgift 5

--- Regex1/Regex1/Program.cs
+++ Regex1/Regex1/Program.cs
@@ -11,16 +11,24 @@ namespace Regex1
     {
         static void Main(string[] args)
         {
-            bool running = true;
-            while (running)
-            {
-                Console.WriteLine("Ange personnummer");
-                string input = Console.ReadLine();
-                Console.WriteLine(Regex.IsMatch(input, "^(19|20)?([0-9]{2}[- ]?[0-9]{2}[- ]?[0-9]{2}[- ]?[0-9]{4})$")
-                    ? "Giltligt!"
-                    : "Ej giltligt, försök igen!");
-            }
+            //// Uppgift 4
+            //bool running = true;
+            //while (running)
+            //{
+            //    Console.WriteLine("Ange personnummer");
+            //    string input = Console.ReadLine();
+            //    Console.WriteLine(Regex.IsMatch(input, "^(19|20)?([0-9]{2}[- ]?[0-9]{2}[- ]?[0-9]{2}[- ]?[0-9]{4})$")
+            //        ? "Giltligt!"
+            //        : "Ej giltligt, försök igen!");
+            //}
 
+            // Uppgift 5
+            while (true)
+            {
+                Console.WriteLine("input text");
+                string input = Console.ReadLine().ToLower();
+                Console.WriteLine(Regex.Matches(input, "\\bdet\\b").Count);
+            }
 
         }
     }
