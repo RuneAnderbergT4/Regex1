6b6f5c08acdaa4c47682c7e0e51bf735d499a730
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-21 10:19:17 +0200

    regex1 uppgift 4

--- Regex1/Regex1/Program.cs
+++ Regex1/Regex1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Regex1
@@ -10,6 +11,17 @@ namespace Regex1
     {
         static void Main(string[] args)
         {
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Ange personnummer");
+                string input = Console.ReadLine();
+                Console.WriteLine(Regex.IsMatch(input, "^(19|20)?([0-9]{2}[- ]?[0-9]{2}[- ]?[0-9]{2}[- ]?[0-9]{4})$")
+                    ? "Giltligt!"
+                    : "Ej giltligt, försök igen!");
+            }
+
+
         }
     }
 }
